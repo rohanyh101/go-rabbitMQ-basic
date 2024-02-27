334f3f73501649426e7531d9232d2237224127b5
only4doma <git4doma@gmail.com>
2024-02-27 13:39:19 +0530

    basic rabbitMQ example in golang

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/rohanyh101/rabbitmq-ex
+
+go 1.21.6
+
+require (
+	github.com/rabbitmq/amqp091-go v1.9.0 // indirect
+	github.com/streadway/amqp v1.1.0 // indirect
+)
---
+++ service-2/main.go
@@ -0,0 +1,65 @@
+// service2.go
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/streadway/amqp"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"service1Queue", // name
+		false,           // durable
+		false,           // delete when unused
+		false,           // exclusive
+		false,           // no-wait
+		nil,             // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	// msgs, err := ch.Consume(
+	// 	q.Name, // queue
+	// 	"",     // consumer
+	// 	true,   // auto-ack
+	// 	false,  // exclusive
+	// 	false,  // no-local
+	// 	false,  // no-wait
+	// 	nil,    // args
+	// )
+	// failOnError(err, "Failed to register a consumer")
+
+	log.Println("Service 2 is up and running")
+
+	for {
+		body := "Hello from Service 2!"
+		err = ch.Publish(
+			"",     // exchange
+			q.Name, // routing key
+			false,  // mandatory
+			false,  // immediate
+			amqp.Publishing{
+				ContentType: "text/plain",
+				Body:        []byte(body),
+			})
+		failOnError(err, "Failed to publish a message")
+
+		log.Printf(" [x] Sent %s", body)
+		time.Sleep(5 * time.Second)
+	}
+}
